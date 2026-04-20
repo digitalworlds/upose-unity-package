098ebeb3308281df56026952317ca8d9cbd71536
daramtulla <92892612+daramtulla@users.noreply.github.com>
2026-04-20 10:38:34 -0400

    Start/Close Mediapipe
    Can start and close "mediapipe" using "StartMediaPipe"

---
+++ Python/run_mediapipe_automatic.py
@@ -0,0 +1,126 @@
+import cv2
+import mediapipe as mp
+from clientUDP_listener import ClientUDP_Listener
+import time
+from upose import UPose
+import os
+
+
+pose_tracker = UPose(source="mediapipe",flipped=True)
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+timeSincePostStatistics = 0
+
+client = ClientUDP_Listener('127.0.0.1',52733)
+client.start()
+
+client2 = ClientUDP_Listener('127.0.0.1',52734)
+client2.start()
+
+client3 = ClientUDP_Listener('127.0.0.1',52735)
+client3.start()
+
+starttime = time.time()
+unitymessage=""
+currmessage=""
+samenumbercounter=0;
+currentreadnumber='0'
+cap = cv2.VideoCapture(0)  # Open webcam
+returnval ="yyyyy"
+endnow = False
+
+returnval=client2.clearbuffer();
+
+
+with mp_pose.Pose(min_detection_confidence=0.80, min_tracking_confidence=0.5, model_complexity = 2, static_image_mode = False,enable_segmentation = True) as pose: 
+    
+    while cap.isOpened():
+
+        
+        ti = time.time()
+        success, image = cap.read()
+
+        
+        if not success:
+            break
+        
+        
+
+        
+
+        image = cv2.flip(image, 1)
+        
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
+        tf = time.time()
+
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.pose_landmarks:
+
+            if time.time()-timeSincePostStatistics>=1:
+                print("Theoretical Maximum FPS: %f"%(1/(tf-ti)))
+                print(returnval)
+                timeSincePostStatistics = time.time()
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+                mp_drawing.DrawingSpec(color=(255, 100, 0), thickness=2, circle_radius=4),
+                mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=2, circle_radius=2),
+            )
+
+        cv2.imshow('MediaPipe Face Detection', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+        
+
+        
+
+        data = "mprot\n"
+        if results.pose_world_landmarks:
+            #Calculate rotations
+            pose_tracker.newFrame(results)
+            pose_tracker.computeRotations();
+            pelvis_rotation = pose_tracker.getPelvisRotation()["local"].as_quat()
+            pelvis_visibility = pose_tracker.getPelvisRotation()["visibility"]
+            torso_rotation = pose_tracker.getTorsoRotation()["local"].as_quat()
+            torso_visibility = pose_tracker.getTorsoRotation()["visibility"]
+            left_shoulder_rotation = pose_tracker.getLeftShoulderRotation()["local"].as_quat()
+            left_shoulder_visibility = pose_tracker.getLeftShoulderRotation()["visibility"]
+            right_shoulder_rotation = pose_tracker.getRightShoulderRotation()["local"].as_quat()
+            right_shoulder_visibility = pose_tracker.getRightShoulderRotation()["visibility"]
+            left_elbow_rotation = pose_tracker.getLeftElbowRotation()["local"].as_quat()
+            left_elbow_visibility = pose_tracker.getLeftElbowRotation()["visibility"]
+            right_elbow_rotation = pose_tracker.getRightElbowRotation()["local"].as_quat()
+            right_elbow_visibility = pose_tracker.getRightElbowRotation()["visibility"]
+            left_hip_rotation = pose_tracker.getLeftHipRotation()["local"].as_quat()
+            left_hip_visibility = pose_tracker.getLeftHipRotation()["visibility"]
+            right_hip_rotation = pose_tracker.getRightHipRotation()["local"].as_quat()
+            right_hip_visibility = pose_tracker.getRightHipRotation()["visibility"]
+            left_knee_rotation = pose_tracker.getLeftKneeRotation()["local"].as_quat()
+            left_knee_visibility = pose_tracker.getLeftKneeRotation()["visibility"]
+            right_knee_rotation = pose_tracker.getRightKneeRotation()["local"].as_quat()
+            right_knee_visibility = pose_tracker.getRightKneeRotation()["visibility"]
+            
+            data += "{}|{}|{}|{}|{}|{}\n".format(0,pelvis_rotation[0],pelvis_rotation[1],pelvis_rotation[2],pelvis_rotation[3],pelvis_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(1,torso_rotation[0],torso_rotation[1],torso_rotation[2],torso_rotation[3],torso_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(2,left_shoulder_rotation[0],left_shoulder_rotation[1],left_shoulder_rotation[2],left_shoulder_rotation[3],left_shoulder_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(3,right_shoulder_rotation[0],right_shoulder_rotation[1],right_shoulder_rotation[2],right_shoulder_rotation[3],right_shoulder_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(4,left_elbow_rotation[0],left_elbow_rotation[1],left_elbow_rotation[2],left_elbow_rotation[3],left_elbow_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(5,right_elbow_rotation[0],right_elbow_rotation[1],right_elbow_rotation[2],right_elbow_rotation[3],right_elbow_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(6,left_hip_rotation[0],left_hip_rotation[1],left_hip_rotation[2],left_hip_rotation[3],left_hip_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(7,right_hip_rotation[0],right_hip_rotation[1],right_hip_rotation[2],right_hip_rotation[3],right_hip_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(8,left_knee_rotation[0],left_knee_rotation[1],left_knee_rotation[2],left_knee_rotation[3],left_knee_visibility)
+            data += "{}|{}|{}|{}|{}|{}\n".format(9,right_knee_rotation[0],right_knee_rotation[1],right_knee_rotation[2],right_knee_rotation[3],right_knee_visibility)
+
+            client.sendMessage(data)
+            client3.sendMessage(os.getppid())
+
+
+            returnval=client2.readMessage();
+            if returnval=="End":
+                break
+                #outward=outward
+            #print(client2.readMessage())
+
+cap.release()
+cv2.destroyAllWindows()
+os._exit
